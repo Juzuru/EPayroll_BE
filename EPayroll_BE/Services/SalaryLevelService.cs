9975c4080de792cd2575d434465be1c0141f6f19
Nhunguyen181 <45573915+Nhunguyen181@users.noreply.github.com>
2019-12-05 19:29:55 +0700

    7:29 PM 2019-12-05
    restful api for SalaryLevels

--- EPayroll_BE/Services/SalaryLevelService.cs
+++ EPayroll_BE/Services/SalaryLevelService.cs
@@ -11,10 +11,13 @@ namespace EPayroll_BE.Services
     public class SalaryLevelService : ISalaryLevelService
     {
         private readonly ISalaryLevelRepository _salaryLevelRepository;
+        private readonly ISalaryTableRepository _salaryTableRepository;
 
-        public SalaryLevelService(ISalaryLevelRepository salaryLevelRepository)
+        public SalaryLevelService(ISalaryLevelRepository salaryLevelRepository,
+            ISalaryTableRepository salaryTableRepository)
         {
             _salaryLevelRepository = salaryLevelRepository;
+            _salaryTableRepository = salaryTableRepository;
         }
 
         public Guid Add(SalaryLevelCreateModel model)
@@ -33,10 +36,41 @@ namespace EPayroll_BE.Services
 
             return salaryLevel.Id;
         }
+
+        public IList<SalaryLevelViewModel> GetAll()
+        {
+            IList<SalaryLevel> list = _salaryLevelRepository.GetAll().OrderByDescending(
+                _salaryLevel => _salaryLevel.Factor).ToList();
+            IList<SalaryLevelViewModel> result = new List<SalaryLevelViewModel>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                SalaryTable salaryTable = _salaryTableRepository.GetById(list[i].SalaryTableId);
+                result.Add(new SalaryLevelViewModel
+                {
+                    Id = list[i].Id,
+                    Condition = list[i].Condition,
+                    Factor = list[i].Factor,
+                    Level = list[i].Level,
+                    Order = list[i].Order,
+                    SalaryTable = new SalaryTableViewModel
+                    {
+                        Id = salaryTable.Id,
+                        Name = salaryTable.Name,
+                        StartDate = salaryTable.StartDate,
+                        EndDate = salaryTable.EndDate,
+                        CreatedDate = salaryTable.CreatedDate,
+                        IsEnable = salaryTable.IsEnable,
+                    }
+                });
+            }
+            return result;
+        }
     }
 
     public interface ISalaryLevelService
     {
         Guid Add(SalaryLevelCreateModel model);
+        IList<SalaryLevelViewModel> GetAll();
     }
 }
