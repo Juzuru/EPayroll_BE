edf4b920a6914907b136f7a3d47f420a1b100daf
Lại Đức Toàn <laiductoan0701@gmail.com>
2019-12-15 15:38:02 +0700

    Update StringGenerationUtility.cs

--- EPayroll_BE/Utilities/StringGenerationUtility.cs
+++ EPayroll_BE/Utilities/StringGenerationUtility.cs
@@ -7,6 +7,8 @@ namespace EPayroll_BE.Utilities
 {
     public class StringGenerationUtility
     {
+        private static readonly string[] alphabet = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+
         private static readonly Random random = new Random();
 
         public static string GenerateCode()
@@ -18,3 +20,5 @@ namespace EPayroll_BE.Utilities
             }
             return result;
         }
+    }
+}
